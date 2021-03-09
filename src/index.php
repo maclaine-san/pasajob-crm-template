01f180f1bc7bdc0da8e6e9e09d4d6d74f37429f7
John Maclaine <johnmaclained@gmail.com>
2021-03-09 18:05:14 +0800

    added charging function

--- src/index.php
+++ src/index.php
@@ -6,6 +6,11 @@
 </head>
 
 <?php 
+ $environment = getenv('environment');
+ $job_post_link = $environment == 'development' ? getenv('dev_job_post_link') : getenv('prod_job_post_link');
+ $charge_customer_link = $environment == 'development' ? getenv('dev_charge_customer_link') : getenv('prod_charge_customer_link');
+ $api_username = $environment == 'development' ? getenv('dev_api_username') : getenv('prod_api_username');
+ $api_key = $environment == 'development' ? getenv('dev_api_key') : getenv('prod_api_key');
  $samplePayload = array(
     'job_title' => 'Payruler test job', 
     'job_description' => 'Sample description', 
@@ -29,7 +34,6 @@
       'value'=> 16
     ], 
     'employer_id' => 4505, 
-    'company_detail_id' => 621, 
     'minimum_qualification' => 'Bachelors Degree', 
     'minimum_experience' => '1 to 2 years', 
     'home_based' => [
@@ -40,7 +44,6 @@
     'skills' => '[{"skill_name":"test","skill_description":"test"}]',
     'vat' => 75,
     'fee' => 200,
-    'transaction_id' => 'abcd',
     'status' => 'Active',
     'visible' => 'true'
  );
@@ -64,11 +67,37 @@
 </div>
 
 <div class="flex justify-center">
-  <button onclick="postJob()"class="bg-blue-400 text-white h-10 w-20 rounded hover:bg-green-400">POST JOB</button>
+  <button onclick="chargeCustomer()"class="bg-blue-400 text-white h-10 w-20 rounded hover:bg-green-400">POST JOB</button>
 </div>
 
 <script type="text/javascript">
-function postJob() {
+
+function chargeCustomer() {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+  var urlencoded = new URLSearchParams();
+  urlencoded.append('name','Sample user name');
+  urlencoded.append('number','Sample Card number');
+  urlencoded.append('exp_month','Sample exp month');
+  urlencoded.append('exp_year','Sample exp year');
+  urlencoded.append('cvc','Sample CVC');
+  urlencoded.append('api_username','<?php echo $api_username;?>');
+  urlencoded.append('api_key','<?php echo $api_key;?>');
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: 'follow'
+  };
+  
+  fetch('<?php echo $charge_customer_link;?>', requestOptions)
+   .then(response => response.json())
+   .then(result => postJob(result.transaction_id))
+   .catch(error => console.log('error', error));
+}
+
+function postJob(transaction_id) {
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
@@ -78,18 +107,22 @@ function postJob() {
       $value = is_array($sample) && array_key_exists('label',$sample) ? $sample['value'] : $sample;
       echo "urlencoded.append('".$key."','".$value."');";
     }
-?>
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: urlencoded,
-  redirect: 'follow'
-};
 
-fetch("https://share.dev.pasajob.com/create-job-web", requestOptions)
-  .then(response => response.text())
-  .then(result => alert(result))
-  .catch(error => console.log('error', error));
+  ?>
+    urlencoded.append("transaction_id",transaction_id);
+    urlencoded.append("api_username",'<?php echo $api_username;?>');
+    urlencoded.append("api_key",'<?php echo $api_key;?>');
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+    
+    fetch('<?php echo $job_post_link;?>', requestOptions)
+      .then(response => response.text())
+      .then(result => alert(result))
+      .catch(error => console.log('error', error));
     
 }
 </script> 
